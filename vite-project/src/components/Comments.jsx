86730b2abe028d4518f2f6c2d071c4ff1b2f2f77
Ankita Goyal <goyalankita0325@gmail.com>
2024-08-08 15:06:05 +0530

    Adding Comments done

--- vite-project/src/components/Comments.jsx
+++ vite-project/src/components/Comments.jsx
@@ -2,21 +2,21 @@ import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import Actions from "./Actions";
-const Comments = () => {
+const Comments = ({userAvatar , comment , createdAt , likes , username}) => {
     const [liked , setLiked] = useState(false);
   return (
    <>
    <Flex gap={"4"} my={'2'} py={"2"} w={"full"}>
-    <Avatar src="/zuck-avatar.png" size={"sm"}/>
+    <Avatar src={userAvatar}size={"sm"}/>
     <Flex gap={"1"} w={"full"} flexDirection={"column"}>
         <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
-            <Text fontSize="sm" fontWeight="bold">Mark</Text>
+            <Text fontSize="sm" fontWeight="bold">{username}</Text>
             <Flex gap={"2"} alignItems={"center"}>
-                <Text fontSize={"sm"} color={"gray.ligth"}>1d</Text>
+                <Text fontSize={"sm"} color={"gray.ligth"}>{createdAt}</Text>
                 <BsThreeDots/>
             </Flex>
         </Flex>
-        <Text>Hey this looks great!</Text>
+        <Text>{comment}</Text>
         <Actions liked={liked} setLiked={setLiked} />
         <Text fontSize={"sm"} color={"gray.light"}>
             {100 + (liked ? 1 : 0)} likes
