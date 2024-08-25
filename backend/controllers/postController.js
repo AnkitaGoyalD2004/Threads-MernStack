b1dfec5497cbf089fe9a0f1f5085ffdbd2784566
Ankita Goyal <goyalankita0325@gmail.com>
2024-08-25 15:59:01 +0530

    Working on post part

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,18 +1,18 @@
 import User from '../models/userModel.js';
 
-const createPost = async (req , res) => {
-try{
-    const {postedBy , text , img} = req.body;
+const createPost = async (req, res) => {
+    try {
+        const { postedBy, text, img } = req.body;
 
-    if(!postedBy || text ){
-        return res.status(400).json({message : "please fill all the fields"});
+        if (!postedBy || text) {
+            return res.status(400).json({ message: "please fill all the fields" });
+        }
+
+        const user = await User.findById(postedBy);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log(error)
     }
-
-const user = await User.findById(postedBy);
-
-}catch(error){
-    res.status(500).json({message : error.message});
-    console.log(error)
 }
-}
-export default {createPost};
+export default { createPost };
