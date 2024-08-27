991244348b9e6bc63da9b5efa603e86e76ef1c95
Ankita Goyal <goyalankita0325@gmail.com>
2024-08-27 12:04:46 +0530

    working on post part

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -5,10 +5,24 @@ const createPost = async (req, res) => {
         const { postedBy, text, img } = req.body;
 
         if (!postedBy || text) {
-            return res.status(400).json({ message: "please fill all the fields" });
+            return res.status(400).json({ message: "postedBy and text fields are required" });
         }
 
         const user = await User.findById(postedBy);
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+
+        }
+        if(user._id.toString() !== req.user._id.toString()){
+            return res.status(401).json({message: "Unauthorized to create post"});
+        }
+
+        const maxLength  = 500;
+        if(text.length > maxLength){
+            return res.status(404).json({message: `Text must be less than ${maxLength} characters`}); 
+        }
+
+       const newPost = new Post({postedBy , text , img})
 
     } catch (error) {
         res.status(500).json({ message: error.message });
