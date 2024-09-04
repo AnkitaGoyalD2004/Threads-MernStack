1e8a6576cadd61649ba9d4f92a3cb849855f139d
Ankita Goyal <goyalankita0325@gmail.com>
2024-09-04 11:30:47 +0530

    done with the post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -121,16 +121,35 @@ const replyToPost = async (req, res) => {
             return res.status(404).json({ message: "Post not found" });
         }
 
-        const reply = {userId , text , userProfilePic , username};
+        const reply = { userId, text, userProfilePic, username };
 
         post.replies.push(reply);
         await post.save();
+
+        res.status(200).json({ message: "Reply added successfully", post });
 
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 }
 
-export { createPost, deletePost, getPost, likeUnlikePost, replyToPost };
+const getFeedPosts = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        const following = user.following;
+
+        const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
+        res.status(200).json({ feedPosts });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export { createPost, deletePost, getFeedPosts, getPost, likeUnlikePost, replyToPost };
 
 
