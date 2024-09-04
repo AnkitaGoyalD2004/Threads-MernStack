22e6276c6a1d51daebb2aec7cb6e711b7f7f7316
Ankita Goyal <goyalankita0325@gmail.com>
2024-09-04 09:48:32 +0530

    working on post part

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -2,77 +2,131 @@ import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
 
 const createPost = async (req, res) => {
-	try {
-		const { postedBy, text } = req.body;
-		let { img } = req.body;
+    try {
+        const { postedBy, text } = req.body;
+        let { img } = req.body;
 
-		if (!postedBy || !text) {
-			return res.status(400).json({ error: "Postedby and text fields are required" });
-		}
+        if (!postedBy || !text) {
+            return res.status(400).json({ error: "Postedby and text fields are required" });
+        }
 
-		const user = await User.findById(postedBy);
-		if (!user) {
-			return res.status(404).json({ error: "User not found" });
-		}
+        const user = await User.findById(postedBy);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
 
-		if (user._id.toString() !== req.user._id.toString()) {
-			return res.status(401).json({ error: "Unauthorized to create post" });
-		}
+        if (user._id.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ error: "Unauthorized to create post" });
+        }
 
-		const maxLength = 500;
-		if (text.length > maxLength) {
-			return res.status(400).json({ error: `Text must be less than ${maxLength} characters` });
-		}
+        const maxLength = 500;
+        if (text.length > maxLength) {
+            return res.status(400).json({ error: `Text must be less than ${maxLength} characters` });
+        }
 
-		if (img) {
-			const uploadedResponse = await cloudinary.uploader.upload(img);
-			img = uploadedResponse.secure_url;
-		}
+        if (img) {
+            const uploadedResponse = await cloudinary.uploader.upload(img);
+            img = uploadedResponse.secure_url;
+        }
 
-		const newPost = new Post({ postedBy, text, img });
-		await newPost.save();
+        const newPost = new Post({ postedBy, text, img });
+        await newPost.save();
 
-		res.status(201).json(newPost);
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-		console.log(err);
-	}
+        res.status(201).json(newPost);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        console.log(err);
+    }
 };
 
 const getPost = async (req, res) => {
-	try {
-		const post = await Post.findById(req.params.id);
+    try {
+        const post = await Post.findById(req.params.id);
 
-		if (!post) {
-			return res.status(404).json({ error: "Post not found" });
-		}
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" });
+        }
 
-		res.status(200).json(post);
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-	}
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
- 
-const deletePost = async(req , res)=>{
- try{
- 
-    const post = await Post.findById(req.params.id);
 
-    if (!post) {
-        return res.status(404).json({ error: "Post not found" });
+const deletePost = async (req, res) => {
+    try {
+
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+        if (post.postedBy.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ message: "Unauthorized to delete post" })
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(500).json({ message: "Post Deleted Successfully" })
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
+};
 
-    if(post.postedBy.toString() !== req.user._id.toString()){
-        return res.status(401).json({message : "Unauthorized to delete post"})
+const likeUnlikePost = async (req, res) => {
+    try {
+
+        const { id: postId } = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({ message: "post not found" })
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+
+        if (userLikedPost) {
+            //unlike Post
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } })
+            res.status(200).json({ message: "post unliked successfully" });
+        } else {
+            //Like Post
+            post.likes.push(userId);
+            await post.save();
+            res.status(500).json({ message: "Post liked successfully" })
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-
-    await Post.findByIdAndDelete(req.params.id);
-    res.status(500).json({message: "Post Deleted Successfully"})
-
- }catch(error){
-    res.status(500).json({ error: error.message });
- }
 }
-export { createPost, deletePost, getPost };
+
+const replyToPost = async (req, res) => {
+    try {
+        const { text } = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+        const userProfilePic = req.user.ProfilePic;
+        const username = req.user.username;
+
+        if (!text) {
+            return res.status(400).json({ message: "Text field is required" })
+        }
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export { createPost, deletePost, getPost, likeUnlikePost, replyToPost };
 
 
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createPost, deletePost, getPost } from "../controllers/postController.js";
+import { createPost, deletePost, getPost, likeUnlikePost, replyToPost } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 const router  = express.Router();
 
 router.get("/:id", getPost)
 router.post("/create" ,protectRoute ,  createPost);
-router.delete("/:id" , protectRoute,deletePost)
+router.delete("/:id" , protectRoute,deletePost);
+router.post("/like/:id" , protectRoute,  likeUnlikePost);
+router.post("/reply/:id" , protectRoute,  replyToPost);
+
 
 export default router;
