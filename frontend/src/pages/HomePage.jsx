0f800bcb1b7d6bb84ce9be9ac637264ab3629531
Ankita Goyal <goyalankita0325@gmail.com>
2024-09-23 11:22:05 +0530

    working on Home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { Flex, Spinner } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import Post from "../components/Post";
 import useShowToast from '../hooks/useShowToast';
-
 const HomePage = () => {
 
     const [posts, setPosts] = useState([]);
@@ -14,31 +14,35 @@ const HomePage = () => {
             try {
                 const res = await fetch("/api/posts/feed");
                 const data = await res.json();
-                if(data.error){
-                    showToast("Error" , data.error , "error");
+                if (data.error) {
+                    showToast("Error", data.error, "error");
                     return;
                 }
                 console.log(data);
                 setPosts(data);
             } catch (error) {
                 showToast("Error", error.message, "error");
-            }finally{
+            } finally {
                 setLoading(false);
             }
         }
         getFeedPosts();
     }, [showToast])
     return (
-      <>
+        <>
 
-{!loading && posts.length === 0 && <h1>Follow some users to see the feed</h1>}
+            {!loading && posts.length === 0 && <h1>Follow some users to see the feed</h1>}
 
-      {loading && (
-        <Flex justify="center">
-            <Spinner size="xl" />
-        </Flex>
-      )}
-      </>
+            {loading && (
+                <Flex justify="center">
+                    <Spinner size="xl" />
+                </Flex>
+            )}
+
+            {posts.map((post) => (
+                <Post key={post._id} post={post} postedBy={post.postedBy} />
+            ))}
+        </>
     )
 }
 
