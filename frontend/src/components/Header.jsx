ba9228d9a842fef3533cc45e2183a9593dfe15f1
Ankita Goyal <goyalankita0325@gmail.com>
2024-10-07 11:14:46 +0530

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,20 @@
 import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
 import { AiFillHome } from "react-icons/ai";
+import { BsFillChatQuoteFill } from "react-icons/bs";
 import { FiLogOut } from "react-icons/fi";
+import { MdOutlineSettings } from "react-icons/md";
 import { RxAvatar } from "react-icons/rx";
 import { Link as RouterLink } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import authScreenAtom from "../atoms/authAtom";
 import userAtom from "../atoms/userAtom";
+import useLogout from "../hooks/useLogout";
 
 const Header = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
 	const user = useRecoilValue(userAtom);
+	const logout = useLogout();
+	const setAuthScreen = useSetRecoilState(authScreenAtom);
 
 	return (
 		<Flex justifyContent={"space-between"} mt={6} mb='12'>
@@ -17,6 +23,11 @@ const Header = () => {
 					<AiFillHome size={24} />
 				</Link>
 			)}
+			{!user && (
+				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("login")}>
+					Login
+				</Link> 
+			 )}
 
 			<Image
 				cursor={"pointer"}
@@ -31,10 +42,22 @@ const Header = () => {
 					<Link as={RouterLink} to={`/${user.username}`}>
 						<RxAvatar size={24} />
 					</Link>
-					<Button size={"xs"}>
-						<FiLogOut size={20}/>
+					<Link as={RouterLink} to={`/chat`}>
+						<BsFillChatQuoteFill size={20} />
+					</Link>
+					<Link as={RouterLink} to={`/settings`}>
+						<MdOutlineSettings size={20} />
+					</Link>
+					<Button size={"xs"} onClick={logout}>
+						<FiLogOut size={20} />
 					</Button>
 				</Flex>
+			)}
+
+			{!user && (
+				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("signup")}>
+					Sign up
+				</Link>
 			)}
 		</Flex>
 	);
