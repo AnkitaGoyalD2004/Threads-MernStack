2d56bf813142390d13a31247ad4f48e3822f7f21
Ankita Goyal <goyalankita0325@gmail.com>
2024-10-17 17:25:27 +0530

    done

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,37 @@
 import { SearchIcon } from '@chakra-ui/icons';
 import { Box, Button, Flex, Input, Skeleton, SkeletonCircle, Text, useColorModeValue } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { conversationsAtom } from '../atoms/messagesAtom';
 import Conversations from '../components/Conversations';
 import MessageContainer from '../components/MessageContainer';
+import useShowToast from "../hooks/useShowToast";
+
 const ChatPage = () => {
+
+      const showToast = useShowToast();
+      const[loadingConversations , setLoadingConversations] = useState(true);
+      const[conversations , setConversations] = useRecoilState(conversationsAtom);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await fetch("/api/messages/conversations");
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error")
+          return;
+        }
+        console.log(data);
+        setConversations(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      }finally{
+        setLoadingConversations(false);
+      }
+    }
+    getConversations();
+  }, [showToast , setConversations]);
   return <Box position={"absolute"} left={"50%"} w={
     {
       base: "100%",
@@ -39,30 +67,31 @@ const ChatPage = () => {
           </Flex>
         </form>
 
-{
-  false && (
-    [0,1,2,3,4].map((_,i) => (
-      <Flex key={i} gap={4} alignItems={"center"} p={"1"} borderRadius={"md"}>
-        <Box>
-          <SkeletonCircle size={"10"} />
-        </Box>
-        <Flex w={"full"} flexDirection={"column"} gap={"3"}>
-          <Skeleton h={"10px"} w={"80px"} />
-          <Skeleton h={"8px"} w={"90%"} />
-          </Flex>
-        </Flex>
-    )))}
-    <Conversations/>
-    <Conversations/>
-    <Conversations/>
-    <Conversations/>
+        {
+          loadingConversations && (
+            [0, 1, 2, 3, 4].map((_, i) => (
+              <Flex key={i} gap={4} alignItems={"center"} p={"1"} borderRadius={"md"}>
+                <Box>
+                  <SkeletonCircle size={"10"} />
+                </Box>
+                <Flex w={"full"} flexDirection={"column"} gap={"3"}>
+                  <Skeleton h={"10px"} w={"80px"} />
+                  <Skeleton h={"8px"} w={"90%"} />
+                </Flex>
+              </Flex>
+            )))}
+      {!loadingConversations && (
+        conversations.map(conversation =>(
+          <Conversations key={conversation._id} conversation = {conversation} />
+        ))
+      ) }
       </Flex>
       {/* <Flex flex={70}
       borderRadius={"md"} p={2} flexDir={"column"} alignItems={"center"} justifyContent={"center"} height={"400px"}>
         <GiConversation size={100} />
         <Text fontSize={20}> Select a conversation to start messaging</Text>
       </Flex> */}
-    <MessageContainer/>
+      <MessageContainer />
     </Flex>
   </Box>
 }
