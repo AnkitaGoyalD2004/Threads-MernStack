b9827542690ab554e213e4f7c1267fc3e89594e8
Ankita Goyal <goyalankita0325@gmail.com>
2024-12-03 22:15:52 +0530

    update

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -55,26 +55,26 @@ async function sendMessage(req, res) {
     }
 }
 
-async function getMessages(req, res) {
-    const { otherUserId } = req.params;
-    const userId = req.user._id;
-    try {
+// async function getMessages(req, res) {
+//     const { otherUserId } = req.params;
+//     const userId = req.user._id;
+//     try {
 
-        const conversation = await Conversation.findOne({
-            participants: { $all: [userId, otherUserId] }
-        })
+//         const conversation = await Conversation.findOne({
+//             participants: { $all: [userId, otherUserId] }
+//         })
 
-if(!conversation){return res.status(404) . json({error : "Conversation not found"})}
+// if(!conversation){return res.status(404) . json({error : "Conversation not found"})}
 
-        const messages = await Message.find({
-            conversationId: conversation._id
-        }).sort({ createdAt: 1 })
-        res.status(200).json(messages);
+//         const messages = await Message.find({
+//             conversationId: conversation._id
+//         }).sort({ createdAt: 1 })
+//         res.status(200).json(messages);
 
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// }
 async function getConversations(req, res) {
 	const userId = req.user._id;
 	try {
